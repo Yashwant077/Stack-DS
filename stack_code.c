e680c44ea2c0a5934d6461b845d861d973b85766
Yashwant077 <84570757+Yashwant077@users.noreply.github.com>
2021-10-03 15:12:09 +0530

    Create stack_code.c
    Incomplete

---
+++ stack_code.c
@@ -0,0 +1,154 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+void setStack(struct Stack* s)
+{
+    s->top = -1;
+    printf("Enter the size for stack::");
+    scanf("%d",&(s->size));
+    printf("Your stack size is %d\n",s->size);
+    s->arr = (int*)malloc((s->size)*sizeof(int));
+    printf("Your Stack set Successfully.\n");
+} 
+
+int isEmpty(struct Stack* s)
+{
+    if(s->top == -1)
+    {
+        printf("\nStack is Empty.\n");
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(struct Stack* s)
+{
+    if(s->top == s->size)
+    {
+        printf("\nStack is Full.\n");
+        return 1;
+    }
+    return 0;
+}
+
+void push(struct Stack* s)
+{
+    if(isFull(s)==1)
+    {
+        return;
+    }
+    int num;
+    printf("There is already %d elements in Stack\n",(s->top)+1);
+    printf("Enter the element you want to push::");
+    scanf("%d",&num);
+    if((s->top) == -1)
+    {
+        s->arr[(s->top)+1] = num;
+        s->top += 1;
+        return;
+    }
+    s->arr[s->top] = num;
+    s->top += 1;
+}
+
+void pop(struct Stack* s)
+{
+    if(isEmpty(s))
+    {
+        return;
+    }
+    else
+    {
+        printf("Size of the Stack is::%d\n",(s->size));
+        printf("Total No. of Elements in the Stack::%d\n",s->top);
+        int val = s->arr[(s->top)-1];
+        s->top -= 1;
+        printf("Top Element %d is popped Successfully.\n",val);
+        printf("Total No. of Elements in the Stack::%d\n",s->top);
+        return;
+    }
+}
+
+void printStack(struct Stack* s)
+{
+    if(isEmpty(s)==1)
+    {
+        return;
+    }
+    printf("Elements in the stack::");
+    for(int i=(s->size); i>0; i--)
+    {
+        printf("\n\t%d::%d",(s->size)-1,s->arr[(s->size)-1]);
+        s->size -= 1;
+    }
+}
+
+void peek(struct Stack* s)
+{
+    if(isEmpty(s)==1)
+        return;
+    int indx;
+    printf("Enter the index no.::");
+    scanf("%d",&indx);
+    if(indx<0)
+    {
+        printf("Enter a Valid index no.::");
+        scanf("%d",&indx);
+    }
+    printf("Element at Index no. %d is:: %d",indx,(s->arr[indx]));
+}
+
+int main()
+{
+    struct Stack* s;
+    int ch;
+    do
+    {
+        printf("\nEnter your choice::"
+               "\n1. Set Stack"
+               "\n2. Push"
+               "\n3. Pop"
+               "\n4. Peek"
+               "\n5. Print Stack"
+               "\n0. Quit\n");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+        case 1:
+            setStack(s);
+            break;
+
+        case 2:
+            push(s);
+            break;
+
+        case 3:
+            pop(s);
+            break;
+
+        case 4:
+            peek(s);
+            break;
+            
+        case 5:
+            printStack(s);
+            break;
+        
+        case 0:
+            exit(1);
+        
+        default:
+            printf("Please Enter a valid choice.");
+            break;
+        }
+    } while (ch != 0);
+    
+    return 0;
+}
